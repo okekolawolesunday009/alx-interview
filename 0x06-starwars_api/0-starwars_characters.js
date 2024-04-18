fa4e50957961ee58633da25ce66b3d174304ba35
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2024-04-18 17:42:07 +0000

    push interview

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const req = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/';
+
+
+req.get(url + id, function (err, res, body) {
+	if (error) {
+		console.log(error);
+	}
+
+	const data = JSON.parse(body);
+	const dd = data.characters;
+	for (const i of dd) {
+		req.get(i, function (err, ress, body1) {
+			if (err) {
+				console.log(err);
+			}
+			const data1 = JSON.parse(body1);
+			console.log(data.name);
+		});
+	}
+});
