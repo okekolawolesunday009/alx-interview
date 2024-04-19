7f5a24942b259fc0e75085e6d04897f0977b923a
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2024-04-19 18:40:20 +0000

    push

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,20 +5,22 @@ const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/';
 
 
-req.get(url + id, function (err, res, body) {
+req.get(url + id, function (error, res, body) {
 	if (error) {
 		console.log(error);
 	}
 
 	const data = JSON.parse(body);
 	const dd = data.characters;
-	for (const i of dd) {
-		req.get(i, function (err, ress, body1) {
-			if (err) {
-				console.log(err);
-			}
-			const data1 = JSON.parse(body1);
-			console.log(data.name);
-		});
-	}
+        for (const i of dd) {
+		 req.get(i, function (err, res, body1) {
+                        if (err) {
+                                console.log(err);
+                        }
+                        const data1 = JSON.parse(body1);
+                        console.log(data1.name);
+                });
+
+        }
+
 });
